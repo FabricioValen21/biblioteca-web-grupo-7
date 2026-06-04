e5faaa077eadbeb3bc3f4c6c0c0107cdd249bd95
Fabricio Valencia <72322695@continental.edu.pe>
2026-06-04 08:08:57 -0500

    Configura servidor Express y conexion a MySQL

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const sequelize = require("./config/database");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("API Biblioteca Web Grupo 7 funcionando");
+});
+
+const PORT = process.env.PORT || 3000;
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conexion a MySQL correcta");
+
+    app.listen(PORT, () => {
+      console.log("Servidor corriendo en http://localhost:" + PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("Error al conectar con MySQL:", error);
+  });
