4315d1f7cd7429590579a5694ea3aa984a7dbbf3
Fabricio Valencia <72322695@continental.edu.pe>
2026-06-05 14:52:09 -0500

    Actualización de la base de datos
    Se agrega un comando para el funcionamiento en phpmyadmin

--- database/biblioteca.sql
+++ database/biblioteca.sql
@@ -1,54 +1,163 @@
 CREATE DATABASE IF NOT EXISTS biblioteca_web;
 USE biblioteca_web;
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-06-2026 a las 05:25:43
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
-DROP TABLE IF EXISTS prestamos;
-DROP TABLE IF EXISTS libros;
-DROP TABLE IF EXISTS usuarios;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    rol ENUM('admin', 'usuario') DEFAULT 'usuario',
-    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
 
-CREATE TABLE libros (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(150) NOT NULL,
-    autor VARCHAR(100) NOT NULL,
-    categoria VARCHAR(80) NOT NULL,
-    cantidad INT NOT NULL DEFAULT 1,
-    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE prestamos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario_id INT NOT NULL,
-    libro_id INT NOT NULL,
-    codigo VARCHAR(20) NOT NULL,
-    fecha_prestamo DATE NOT NULL,
-    fecha_devolucion DATE NULL,
-    estado ENUM('pendiente', 'prestado', 'devuelto') DEFAULT 'pendiente',
-    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+--
+-- Base de datos: `biblioteca_web`
+--
 
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
+-- --------------------------------------------------------
 
-    FOREIGN KEY (libro_id) REFERENCES libros(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
+--
+-- Estructura de tabla para la tabla `libros`
+--
 
-INSERT INTO libros (titulo, autor, categoria, cantidad)
-VALUES
-('Cien años de soledad', 'Gabriel García Márquez', 'Novela', 5),
-('El principito', 'Antoine de Saint-Exupéry', 'Literatura', 3),
-('Clean Code', 'Robert C. Martin', 'Programación', 2),
-('Don Quijote de la Mancha', 'Miguel de Cervantes', 'Clásico', 4),
-('Introducción a JavaScript', 'Autor académico', 'Programación Web', 6);
+CREATE TABLE `libros` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(150) NOT NULL,
+  `autor` varchar(100) NOT NULL,
+  `categoria` varchar(80) NOT NULL,
+  `cantidad` int(11) NOT NULL DEFAULT 1,
+  `createdAt` datetime DEFAULT current_timestamp(),
+  `updatedAt` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `libros`
+--
+
+INSERT INTO `libros` (`id`, `titulo`, `autor`, `categoria`, `cantidad`, `createdAt`, `updatedAt`) VALUES
+(1, 'Cien años de soledad', 'Gabriel García Márquez', 'Novela', 5, '2026-06-04 08:06:26', '2026-06-04 13:41:01'),
+(2, 'El principito', 'Antoine de Saint-Exupéry', 'Literatura', 3, '2026-06-04 08:06:26', '2026-06-04 08:06:26'),
+(3, 'Clean Code', 'Robert C. Martin', 'Programación', 2, '2026-06-04 08:06:26', '2026-06-04 08:06:26'),
+(4, 'Don Quijote de la Mancha', 'Miguel de Cervantes', 'Clásico', 4, '2026-06-04 08:06:26', '2026-06-05 03:11:53'),
+(5, 'Introducción a JavaScript', 'Autor académico', 'Programación Web', 6, '2026-06-04 08:06:26', '2026-06-04 08:06:26');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamos`
+--
+
+CREATE TABLE `prestamos` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `libro_id` int(11) NOT NULL,
+  `codigo` varchar(20) NOT NULL,
+  `fecha_prestamo` date NOT NULL,
+  `fecha_devolucion` date DEFAULT NULL,
+  `estado` enum('pendiente','prestado','devuelto') DEFAULT 'pendiente',
+  `createdAt` datetime DEFAULT current_timestamp(),
+  `updatedAt` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `prestamos`
+--
+
+INSERT INTO `prestamos` (`id`, `usuario_id`, `libro_id`, `codigo`, `fecha_prestamo`, `fecha_devolucion`, `estado`, `createdAt`, `updatedAt`) VALUES
+(1, 1, 1, '6F166BD9', '2026-06-04', '2026-06-04', 'devuelto', '2026-06-04 13:40:38', '2026-06-04 13:41:01'),
+(2, 2, 4, '2EF3CD02', '2026-06-04', '2026-06-04', 'devuelto', '2026-06-05 03:05:54', '2026-06-05 03:11:53');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` enum('admin','usuario') DEFAULT 'usuario',
+  `createdAt` datetime DEFAULT current_timestamp(),
+  `updatedAt` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `email`, `password`, `rol`, `createdAt`, `updatedAt`) VALUES
+(1, 'Fabricio', 'fabricio@gmail.com', '$2b$10$6j2Za7Jae.C9pZZX3ueU2ed2R0WYiwfrY3gTzQNB1PYw0X8GZELp6', 'admin', '2026-06-04 13:24:44', '2026-06-04 08:26:44'),
+(2, 'Shiara Rodriguez', 'Shiara@gmail.com', '$2b$10$VFgikfyHSQE.V8J/PVGvuutA6xw.DE2mnf8jkf2KpSqs6.n9nNp7.', 'usuario', '2026-06-05 03:01:40', '2026-06-05 03:01:40');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `libros`
+--
+ALTER TABLE `libros`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `libro_id` (`libro_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `libros`
+--
+ALTER TABLE `libros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`libro_id`) REFERENCES `libros` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
